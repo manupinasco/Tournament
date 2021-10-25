d0b3424a53a4d4960c63809814718a51dc7c6f19
silibestref <federicosilibestre@gmail.com>
2021-10-24 22:41:47 -0300

    Inicio y efectos

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace TP_NT
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
