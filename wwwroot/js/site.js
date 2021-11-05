7aa3cf55cbd91f3436b896f5103371ce58b5fe4b
NeverwhereNowhere <manpinas14@gmail.com>
2021-11-04 21:47:51 -0300

    Crear Equipo

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,19 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Write your JavaScript code. 
+function ValidateJugSelection()  
+{  
+    var checkboxes = document.getElementsByName("jug");  
+    var numberOfCheckedItems = 0;  
+    for(var i = 0; i < checkboxes.length; i++)  
+    {  
+        if(checkboxes[i].checked)  
+            numberOfCheckedItems++;  
+    }  
+    if(numberOfCheckedItems > 2)  
+    {  
+        alert("You can't select more than two favorite pets!");  
+        return false;  
+    }  
+} 
