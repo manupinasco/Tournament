dd052f7e51706842eb55a49037a1f0466510611e
NeverwhereNowhere <manpinas14@gmail.com>
2021-11-08 09:25:31 -0300

    Funcionalidad Crear Equipo

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,62 +5,26 @@
 var usuarioPlata = 0;
 let seleccionados = []; 
 
-function ValidateJugSelection(posicion, contrato, idJug)  
+function ValidateJugSelection(objeto, contrato, nombre)  
 {  
-    /*var checkboxes = document.getElementsById(posicion);
-    var numberOfCheckedItems = 0;  
-    for(var i = 0; i < checkboxes.length; i++)  
-    {  
-        if(checkboxes[i].checked) {
-            numberOfCheckedItems++;  
-        }  
-    }  */
-    if(seleccionados.indexOf(idJug) != -1) {
-        seleccionados.splice(seleccionados.indexOf(idJug), 1);
+    if(seleccionados.indexOf(nombre) != -1) {
+        seleccionados.splice(seleccionados.indexOf(nombre), 1);
         usuarioPlata = parseFloat(document.getElementById("plata").innerHTML) + parseFloat(contrato);
         document.getElementById("plata").innerHTML = usuarioPlata;
     }
     else {
         usuarioPlata = document.getElementById("plata").innerHTML - parseFloat(contrato);
-        if(usuarioPlata < 0 /*|| numberOfCheckedItems != 1*/) {
+        if(usuarioPlata < 0) {
             
             alert("¡No podés seleccionarlo!");   
-            document.getElementById(posicion).checked = 0;
+            objeto.checked = 0;
         }
         else {
             document.getElementById("plata").innerHTML = usuarioPlata;
-            seleccionados.push(idJug);
+            seleccionados.push(nombre);
             
         }
     }
     
     
 }   
-function ValidateJugSelectionSup(posicion, contrato, idJug)  
-{  
-    /*var checkboxes = document.getElementsById(posicion);
-    var numberOfCheckedItems = 0;  
-    for(var i = 0; i < checkboxes.length; i++)  
-    {  
-        if(checkboxes[i].checked) {
-            numberOfCheckedItems++;  
-        }  
-    }  */
-    if(seleccionados.indexOf(idJug) != -1) {
-        seleccionados.splice(seleccionados.indexOf(idJug), 1);
-        usuarioPlata = parseFloat(document.getElementById("plata").innerHTML) + parseFloat(contrato);
-        document.getElementById("plata").innerHTML = usuarioPlata;
-    }
-    else {
-        usuarioPlata = document.getElementById("plata").innerHTML - parseFloat(contrato);
-        if(usuarioPlata < 0 /*|| numberOfCheckedItems != 1*/) {
-            alert("¡No podés seleccionarlo!");
-            posicion.checked = 0;
-        }
-        else {
-            document.getElementById("plata").innerHTML = usuarioPlata;
-            seleccionados.push(idJug);
-            
-        }
-    }
-}  
