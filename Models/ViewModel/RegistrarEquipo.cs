1f6ee9407dc28302894dbb7461bedbc6876ac344
silibestref <federicosilibestre@gmail.com>
2021-11-09 02:37:13 -0300

    Se agregan sessiones

---
+++ Models/ViewModel/RegistrarEquipo.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TP_NT.Models.ViewModel
+{
+    public class RegistrarEquipo
+    {
+    public IEnumerable<SelectListItem> Equipos { get; set; }        
+
+    }
+}
